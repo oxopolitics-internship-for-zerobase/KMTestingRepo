5b9c97c1aaeff121d1fb16da65b6125725462fda
Jung YoungHun <56586322+young-st511@users.noreply.github.com>
2022-07-26 22:58:38 +0900

    Update App.jsx (#13)

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import React, { useState } from 'react'
-import './App.css'
-import { faCheckSquare, faSpinner, faBars } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from "react";
+import "./App.css";
+import {
+  faCheckSquare,
+  faSpinner,
+  faBars,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import TestCompo from './components/TestCompo'
+import TestCompo from "./components/TestCompo";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+  const [count3, setCount3] = useState(0);
   return (
-    <div className="App">
-      <div>ddd{count}</div>
-      <FontAwesomeIcon icon={faBars} />
-      <TestCompo />
-      <p>tltltltl</p>
-    </div>
-  )
+    <>
+      <div className="App">
+        <div>ddd{count}</div>
+        <FontAwesomeIcon icon={faBars} />
+        <TestCompo />
+        <p>tltltltl</p>
+      </div>
+
+      <div className="card">
+        <button onClick={() => setCount2((count2) => count2 + 1)}>
+          count is {count2}
+        </button>
+        <button onClick={() => setCount3((count3) => count3 - 1)}>
+          count is {count3}
+        </button>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
