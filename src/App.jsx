f851b446b361113fb8f1b167cda018da2a17822b
Jung YoungHun <asdk511@gmail.com>
2022-07-27 12:13:00 +0900

    update: 충돌 만들기!
    충돌이 날까요?

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import React, { useState } from "react";
+// 주석!
 import "./App.css";
+// 주석!
 import {
+  // 주석!
   faCheckSquare,
+  // 주석!
   faSpinner,
+  // 주석!
   faBars,
 } from "@fortawesome/free-solid-svg-icons";
+// 주석!
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// 주석!
 import TestCompo from "./components/TestCompo";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [count2, setCount2] = useState(0);
-  const [count3, setCount3] = useState(0);
+  const [ccount2, setCCount2] = useState(0);
+  const [ccount3, setCCount3] = useState(0);
   return (
     <>
       <div className="App">
-        <div>ddd{count}</div>
         <FontAwesomeIcon icon={faBars} />
         <TestCompo />
         <p>tltltltl</p>
       </div>
 
-      <div className="card">
-        <button onClick={() => setCount2((count2) => count2 + 1)}>
-          count is {count2}
+      <div className="cards">
+        <button onClick={() => setCCount2((ccount2) => ccount2 + 1)}>
+          count is {ccount2}
         </button>
-        <button onClick={() => setCount3((count3) => count3 - 1)}>
-          count is {count3}
+        <button onClick={() => setCCount3((ccount3) => ccount3 - 1)}>
+          count is {ccount3}
         </button>
+      </div>
+      <div>
+        c1: {ccount2} / c2: {ccount3}
       </div>
     </>
   );
