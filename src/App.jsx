1a433a9159f611f6fda181c439df62d0d5b612b9
Jung YoungHun <asdk511@gmail.com>
2022-07-28 16:44:24 +0900

    ㅉㅉ

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,39 @@
 import React, { useState } from "react";
-// 주석!
 import "./App.css";
-// 주석!
 import {
-  // 주석!
   faCheckSquare,
-  // 주석!
   faSpinner,
-  // 주석!
   faBars,
 } from "@fortawesome/free-solid-svg-icons";
-// 주석!
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// 주석!
 import TestCompo from "./components/TestCompo";
 
 function App() {
-  const [ccount2, setCCount2] = useState(0);
-  const [ccount3, setCCount3] = useState(0);
-  return (
+  const [count11, setCount11] = useState(0);
+  const [count112, setCount112] = useState(0);
+  const [count113, setCount113] = useState(0);
+  return ( 
     <>
-      <div className="App">
-        <FontAwesomeIcon icon={faBars} />
-        <TestCompo />
-        <p>tltltltl</p>
-      </div>
-
-      <div className="cards">
-        <button onClick={() => setCCount2((ccount2) => ccount2 + 1)}>
-          count is {ccount2}
-        </button>
-        <button onClick={() => setCCount3((ccount3) => ccount3 - 1)}>
-          count is {ccount3}
-        </button>
+      <div>
+        이소윤 충돌발생 중입니다!
       </div>
       <div>
-        c1: {ccount2} / c2: {ccount3}
+        이소윤 충돌발생 중입니다!
+      </div>
+      <div>
+        이소윤 충돌발생 중입니다!
+      </div>
+      <div>
+        이소윤 충돌발생 중입니다!
+      </div>
+      <div>
+        이소윤 충돌발생 중입니다!
+      </div>
+      <div>
+        이소윤 충돌발생 중입니다!
+      </div>
+      <div>
+        이소윤 충돌발생 중입니다!
       </div>
     </>
   );
